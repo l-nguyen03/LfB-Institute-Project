f38fabf2c3e4545674e641ebd88dcd275095e1c5
Pham Tuan Linh Nguyen <pham.tuan.linh.nguyen@rwth-aachen.de>
2023-04-25 15:50:11 +0200

    Add Outline

---
+++ cheat_detection.py
@@ -0,0 +1,53 @@
+
+'''
+Describe concisely about what the function does and specify:
+
+<Argument Name>: <Argument Type>
+
+<Return Variable>: <Return Type>
+
+'''
+def face_comparison():
+    pass
+
+
+
+'''
+Describe concisely about what the function does and specify:
+
+<Argument Name>: <Argument Type>
+
+<Return Variable>: <Return Type>
+
+'''
+def face_counter():
+    pass
+
+
+
+'''
+Describe concisely about what the function does and specify: 
+
+<Argument Name>: <Argument Type>
+
+<Return Variable>: <Return Type>
+
+'''
+def audio_detection():
+    pass
+
+
+'''
+Describe concisely about what the function does and specify:
+
+<Argument Name>: <Argument Type>
+
+<Return Variable>: <Return Type>
+
+'''
+def prepare_message():
+    pass
+
+
+if __name__ == "__main__":
+    pass
