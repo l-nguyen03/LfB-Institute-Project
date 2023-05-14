d1e068910c75d24a0dd6c05ce96743f526fe418f
Pham Tuan Linh Nguyen <pham.tuan.linh.nguyen@rwth-aachen.de>
2023-05-14 19:22:14 +0200

    Suggestion added

--- cheat_detection.py
+++ cheat_detection.py
@@ -1,15 +1,41 @@
+import numpy as np
+import cv2 as cv
+from faceRecognition import camera_monitor
+
 
 '''
-Describe concisely about what the function does and specify:
+The function captures a frame using OpenCV and pass
+the frame to another function to detect whether the student 
+cheats or not. If the student cheats, the captured 
 
 <Argument Name>: <Argument Type>
+void
 
 <Return Variable>: <Return Type>
-
+void
 '''
-def face_comparison():
-    pass
+def face_monitor():
+    cap = cv.VideoCapture(0)
+    if not cap.isOpened():
+        print("Error opening video capture")
+        return
+    
+    ret, frame = cap.read()
+    cheat = camera_monitor(frame)
+    if cheat: 
+        # Define the codec and create VideoWriter object
+        fourcc = cv.VideoWriter_fourcc(*'XVID')
+        out = cv.VideoWriter('output.avi', fourcc, 20.0, frame.shape[:2][::-1])
 
+        # Vertically flip the frame
+        frame = cv.flip(frame, 1)
+
+        # Write the flipped frame
+        out.write(frame)
+        out.release()
+
+    cap.release()
+    cv.destroyAllWindows()
 
 
 '''
@@ -50,4 +76,11 @@ def prepare_message():
 
 
 if __name__ == "__main__":
-    pass
+
+    while True:
+        try:
+            face_monitor()
+            
+        except KeyboardInterrupt:
+            break
+
--- faceRecognition.py
+++ faceRecognition.py
@@ -3,36 +3,57 @@ import cv2 as cv
 import face_recognition as fr
 import numpy as np
 import time
+import os
+
+dir_path = os.path.dirname(os.path.abspath(__file__))
+model_path = os.path.join(dir_path, "shape_predictor_81_face_landmarks.dat")
+jpg_dir_path = os.path.join(dir_path, "faces")
+
 
 # Initialize face detector and shape predictor
 detector = dlib.get_frontal_face_detector()
-predictor = dlib.shape_predictor("/home/ali/Institutsprojekt/shape_predictor_81_face_landmarks.dat")  # Path for shape_predictor
+
+# Path for shape_predictor
+predictor = dlib.shape_predictor(model_path)
 
 # Load the known face image and encode it
-student_img = fr.load_image_file("/home/ali/Institutsprojekt/faces/student.jpg")  # Path for the known faces
-student_face_encoding = fr.face_encodings(student_img)[0]
 
-known_faces = [student_face_encoding]  # List of known face encodings
+# Path for the known face
+student_img = [fr.load_image_file(os.path.join(jpg_dir_path, jpg_file)) for jpg_file in os.listdir(jpg_dir_path) if ".jpg" in jpg_file]
+
+# List of known face encodings
+known_face_encoding = [fr.face_encodings(student_image)[0] for student_image in student_img]
 known_names = ["Student"]  # List of known names
 
-# Initialize the camera
-capture = cv.VideoCapture(0)
 
-unknown_face_start_time = None
-second_person_detected = False
 
-while True:
-    ret, img = capture.read()
-    if not ret:
-        break
+'''
+Function for monitoring the examinee camera during exam.
+The function receives a captured frame and count how many faces
+are present and then perform identity check. 
+
+<Argument> : <Argument Type>
+img : Frame captured by using cv.capture
+
+<Return> : <Return Type>
+cheat or not? : bool 
+'''
+def camera_monitor(img):
+    unknown_face_start_time = None
 
     # Convert the image to grayscale for better face detection
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
     faces = detector(gray)
 
+    #Check if more faces or no face present
     if len(faces) > 1:
-        second_person_detected = True
+        print("More than one face detected!")
+        return True
+    elif len(faces) == 0: 
+        print("Face out of frame!")
+        return True
 
+    #If only one face present then proceed to identity check
     for face in faces:
         landmarks = predictor(gray, face)
 
@@ -47,30 +68,28 @@ while True:
         face_img_rgb = cv.cvtColor(face_img, cv.COLOR_BGR2RGB)
 
         # Compare the face with the known faces
-        face_encodings = fr.face_encodings(face_img_rgb)
+        face_encoding = fr.face_encodings(face_img_rgb)
         name = "Unknown"
         accuracy = 0
 
-        if len(face_encodings) > 0:
-            face_encoding = face_encodings[0]
-            results = fr.compare_faces(known_faces, face_encoding)
+        if len(face_encoding) > 0:
+            face_encoding = face_encoding[0]
+            results = fr.compare_faces(known_face_encoding, face_encoding)
 
             if True in results:
                 matched_index = results.index(True)
                 name = known_names[matched_index]
-                face_distances = fr.face_distance(known_faces, face_encoding)
+                face_distances = fr.face_distance(known_face_encoding, face_encoding)
                 accuracy = round((1 - face_distances[matched_index]) * 100, 2)
 
-            if name == "Unknown" and unknown_face_start_time is None:
+            if name == "Unknown":
                 unknown_face_start_time = time.time()
-            elif name != "Unknown":
-                unknown_face_start_time = None
 
         # Draw a bounding box around the face and write the name below it
         cv.putText(img, f"{name} ({accuracy}%)", (x, y - 10), cv.FONT_HERSHEY_SIMPLEX, 0.9, (0, 0, 255), 2)
 
         # Draw landmarks on the face
-        for n in range(68):
+        for n in range(81):
             x, y = landmarks.part(n).x, landmarks.part(n).y
             cv.circle(img, (x, y), 2, (0, 255, 0), -1)
 
@@ -78,15 +97,8 @@ while True:
     cv.imshow("Camera", img)
 
     # Print messages
-    if unknown_face_start_time is not None and time.time() - unknown_face_start_time > 5:
+    if unknown_face_start_time is not None:
         print("Unknown face")
-    if second_person_detected:
-        print("Second person detected")
-        second_person_detected = False
-
-    # Close the program with 'q'
-    if cv.waitKey(1) == ord("q"):
-        break
-
-capture.release()
-cv.destroyAllWindows()
+        return True
+    
+    return False
